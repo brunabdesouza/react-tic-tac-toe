9a2a8eaccedbc200ab1948ac9353f3a8346a18dd
Bruna Barbosa de Souza <bruna.bdesouza@gmail.com>
2020-12-21 13:37:53 +1100

    first update

---
+++ src/TicTacToe.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class TicTacToe extends React.Component {
+
+  render() {
+    return (
+      <div>
+
+        <h1>Tic Tac Toe</h1>
+      </div>
+
+    );
+
+
+
+  } // render
+
+
+
+
+}; // class TicTacToe
+
+export default TicTacToe;
